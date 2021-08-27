d1203e4ba4b44cfd152d75aedb294b47f0ec7d3d
elmi <shahrokhelmi@gmail.com>
2021-08-27 18:19:49 +0430

    The 7th video has started

--- 2/3/play.js
+++ 2/3/play.js
@@ -1,19 +1,30 @@
-var name = 'Shahrokh';
-var age = 28;
-var hasHobies = true;
+// var name = 'Shahrokh';
+// var age = 28;
+// var hasHobies = true;
 
 
-const summarizeUser = function (name, age, hasHobies) {
-    return (
-        'name is ' + name
-        + ' age: ' + age
-        + ' has hobbies: ' + hasHobies
-    )
+// const summarizeUser = function (name, age, hasHobies) {
+//     return (
+//         'name is ' + name
+//         + ' age: ' + age
+//         + ' has hobbies: ' + hasHobies
+//     )
+// }
+
+// const add = (a, b) => a + b;
+// const addOne = a => a + 1
+
+// console.log(add(1, 2))
+// console.log(addOne(4))
+// // console.log(summarizeUser(name, age, hasHobies))
+
+const person = {
+    name: 'shahrokh',
+    age: '28',
+    greetin: function () {
+        console.log(this.age)
+    },
+
 }
 
-const add = (a, b) => a + b;
-const addOne = a => a + 1
-
-console.log(add(1, 2))
-console.log(addOne(4))
-// console.log(summarizeUser(name, age, hasHobies))
+person.greetin()
