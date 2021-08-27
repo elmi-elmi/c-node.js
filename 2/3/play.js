5d63eff39a611527777c489c8486144fb9ff74c9
elmi <shahrokhelmi@gmail.com>
2021-08-27 17:27:20 +0430

    This is the first commit.

---
+++ 2/3/play.js
@@ -0,0 +1,13 @@
+var name = 'Shahrokh';
+var age = 28;
+var hasHobies = true;
+
+
+function summerizeUser(name, age, hasHobies) {
+    return (
+        'name is ' + name
+        + ' age: ' + age
+        + ' has hobbies: ' + hasHobies
+    )
+}
+console.log(summerizeUser(name, age, hasHobies))
