0ec5405bb29f2721d24ca52f73a3e096bbcc4f92
elmi <shahrokhelmi@gmail.com>
2021-08-27 18:11:31 +0430

    The 6th video has started

--- 2/3/play.js
+++ 2/3/play.js
@@ -3,11 +3,17 @@ var age = 28;
 var hasHobies = true;
 
 
-function summarizeUser(name, age, hasHobies) {
+const summarizeUser = function (name, age, hasHobies) {
     return (
         'name is ' + name
         + ' age: ' + age
         + ' has hobbies: ' + hasHobies
     )
 }
-console.log(summarizeUser(name, age, hasHobies))
+
+const add = (a, b) => a + b;
+const addOne = a => a + 1
+
+console.log(add(1, 2))
+console.log(addOne(4))
+// console.log(summarizeUser(name, age, hasHobies))
